3aae7e7432b5dd35210a8ad0b14724a6ff47ba0b
webmass <webmass@inverse.finance>
2025-03-20 18:03:50 +0400

    update header txt colors

--- app/stable-yields/page.tsx
+++ app/stable-yields/page.tsx
@@ -7,13 +7,13 @@ export default async function YieldsPage() {
   const json = await data.json();
   return (
     <>
-      <header className="flex-wrap items-center justify-center py-4">
+      <header className="flex-wrap items-center justify-center py-12">
         {/* <AppNav activeItem="/stable-yields" /> */}
         <div className="text-center">
-          <h1 className="text-8xl font-bold text-blue-200 dark:text-blue-100 mb-2">
+          <h1 className="text-8xl font-bold text-gray-100 dark:text-blue-100 mb-2">
             Stable Yields
           </h1>
-          <p className="text-2xl text-blue-400 dark:text-gray-400">
+          <p className="text-2xl text-gray-300 dark:text-gray-400">
             Compare stablecoin yields across major DeFi protocols
           </p>
         </div>
