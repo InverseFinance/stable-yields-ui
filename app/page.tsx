87d09ad0ebdcd653313036ca9ba88770ccff2984
webmass <webmass@inverse.finance>
2025-03-20 17:58:07 +0400

    Cache: 5 min

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
 import YieldsPage from "./stable-yields/page";
 
+export const revalidate = 300;
+
 export default YieldsPage;
--- app/stable-yields/page.tsx
+++ app/stable-yields/page.tsx
@@ -1,23 +1,24 @@
-import { AppNav } from "@/components/app-nav";
 import { YieldTable } from "@/components/yield-table";
+
+export const revalidate = 300;
 
 export default async function YieldsPage() {
   const data = await fetch(`https://inverse.finance/api/dola/sdola-comparator`);
   const json = await data.json();
   return (
     <>
-      <header className="flex-wrap items-center justify-center py-15">
+      <header className="flex-wrap items-center justify-center py-4">
         {/* <AppNav activeItem="/stable-yields" /> */}
-        <div className="text-center gap-2">
-          <h1 className="text-6xl font-extrabold text-gray-200 dark:text-blue-100">
+        <div className="text-center">
+          <h1 className="text-8xl font-bold text-blue-200 dark:text-blue-100 mb-2">
             Stable Yields
           </h1>
-          <p className="text-xl font-bold text-gray-300/80 dark:text-gray-400">
+          <p className="text-2xl text-blue-400 dark:text-gray-400">
             Compare stablecoin yields across major DeFi protocols
           </p>
         </div>
       </header>
-      <div className="flex flex-col gap-4 w-full">
+      <div className="flex flex-col gap-4 w-full items-center justify-center">
         <YieldTable data={json.rates} timestamp={json.timestamp} />
       </div>
     </>
