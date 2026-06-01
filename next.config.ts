464b7abd18c69a1661aa0fa738f9a32dd653814c
webmass <webmass@inverse.finance>
2026-06-01 16:03:36 +0400

    images: add domain

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['images.ctfassets.net', 'inverse.finance', 'assets.coingecko.com', 'cdn.jsdelivr.net', 'icons.llamao.fi', 'coin-images.coingecko.com', 'token-icons.llamao.fi', 'raw.githubusercontent.com', 'resources.curve.finance', 'token-assets-one.vercel.app'],
+    domains: ['images.ctfassets.net', 'inverse.finance', 'assets.coingecko.com', 'cdn.jsdelivr.net', 'icons.llamao.fi', 'coin-images.coingecko.com', 'token-icons.llamao.fi', 'raw.githubusercontent.com', 'resources.curve.finance', 'token-assets-one.vercel.app', 'yearn.fi'],
     // loader: 'custom',
     // loaderFile: './components/ui/image.tsx',
   },
